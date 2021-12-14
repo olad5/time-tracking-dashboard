7bd78dcc71e20dedef29939741c3d6a5641cee43
Francis Shonubi <shonubioladipo@gmail.com>
2021-12-14 01:18:21 +0100

    feat: Added the basic markup for the dashboard
    - Added Html markup in App.js to get a feel of how the whole
    app would be structured.
    - imported the images and svg files in the src folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import exerciseSvg from './images/icon-exercise.svg';
+import user_img from './images/image-jeremy.png';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <main>
+        <div class="user-card">
+          <div class="user-info">
+            {/* <img class="user-photo" src='image-jeremy.png' alt="User Profile " /> */}
+            <img class="user-photo" src={user_img} alt="User Profile " />
+            <p>Report for</p>
+            <p className="user-name">Jeremy Robson</p>
+          </div>
+          <div className="timeline">
+            <p className="daily timeline-description">Daily</p>
+            <p className="weekly timeline-description">Weekly</p>
+            <p className="monthly timeline-description">Monthly</p>
+          </div>
+        </div>
+        <div className="stats-card">
+          <img class="svg-container" src={exerciseSvg} alt="User Profile " />
+          <div className="stats-box">
+            <div className="stats-menu">...</div>
+            <p className="activity">Work</p>
+            <p className="current-stats">32hrs</p>
+            <p className="prev-stats">Last Week - 10hrs</p>
+          </div>
+        </div>
+      </main>
+
     </div>
   );
 }
