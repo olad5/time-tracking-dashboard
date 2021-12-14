7cdce6d88afd051f051e964e0ab3d73c8abf6cd7
Francis Shonubi <shonubioladipo@gmail.com>
2021-12-14 21:42:11 +0100

    style: Add styling to user card
    - Added styling for user card
    - Added global css variables for colors and fonts

--- src/App.js
+++ src/App.js
@@ -9,26 +9,29 @@ function App() {
     <div className="App">
 
       <main>
+        {/* user card */}
         <div class="user-card">
           <div class="user-info">
             {/* <img class="user-photo" src='image-jeremy.png' alt="User Profile " /> */}
             <img class="user-photo" src={user_img} alt="User Profile " />
-            <p>Report for</p>
+            <p class='report-text'>Report for</p>
             <p className="user-name">Jeremy Robson</p>
           </div>
-          <div className="timeline">
-            <p className="daily timeline-description">Daily</p>
-            <p className="weekly timeline-description">Weekly</p>
-            <p className="monthly timeline-description">Monthly</p>
-          </div>
+          <p className="daily timeline-description">Daily</p>
+          <p className="weekly timeline-description">Weekly</p>
+          <p className="monthly timeline-description">Monthly</p>
         </div>
-        <div className="stats-card">
-          <img class="svg-container" src={exerciseSvg} alt="User Profile " />
-          <div className="stats-box">
-            <div className="stats-menu">...</div>
-            <p className="activity">Work</p>
-            <p className="current-stats">32hrs</p>
-            <p className="prev-stats">Last Week - 10hrs</p>
+
+        <div className="stats-cards-container">
+          {/* each of the stats cards */}
+          <div className="stats-card">
+            <img class="svg-container" src={exerciseSvg} alt="User Profile " />
+            <div className="stats-box">
+              <div className="stats-menu">...</div>
+              <p className="activity">Work</p>
+              <p className="current-stats">32hrs</p>
+              <p className="prev-stats">Last Week - 10hrs</p>
+            </div>
           </div>
         </div>
       </main>
