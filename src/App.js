cf37739fb3af573f726410c2f97998cd39eb8f14
Francis Shonubi <shonubioladipo@gmail.com>
2021-12-15 13:21:36 +0100

    style: Add styling for stats card
    - Added Basic styling template for each of the activities
    - changed class to ClassName in the App.js, class is a reserved word in
      js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import exerciseSvg from './images/icon-exercise.svg';
 import user_img from './images/image-jeremy.png';
 
@@ -10,11 +9,11 @@ function App() {
 
       <main>
         {/* user card */}
-        <div class="user-card">
-          <div class="user-info">
-            {/* <img class="user-photo" src='image-jeremy.png' alt="User Profile " /> */}
-            <img class="user-photo" src={user_img} alt="User Profile " />
-            <p class='report-text'>Report for</p>
+        <div className="user-card">
+          <div className="user-info">
+            {/* <img className="user-photo" src='image-jeremy.png' alt="User Profile " /> */}
+            <img className="user-photo" src={user_img} alt="User Profile " />
+            <p className='report-text'>Report for</p>
             <p className="user-name">Jeremy Robson</p>
           </div>
           <p className="daily timeline-description">Daily</p>
@@ -25,10 +24,19 @@ function App() {
         <div className="stats-cards-container">
           {/* each of the stats cards */}
           <div className="stats-card">
-            <img class="svg-container" src={exerciseSvg} alt="User Profile " />
+            <img className="svg-icon" src={exerciseSvg} alt="User Profile " />
             <div className="stats-box">
               <div className="stats-menu">...</div>
-              <p className="activity">Work</p>
+              <p className="activity">Exercise</p>
+              <p className="current-stats">32hrs</p>
+              <p className="prev-stats">Last Week - 10hrs</p>
+            </div>
+          </div>
+          <div className="stats-card">
+            <img className="svg-icon" src={exerciseSvg} alt="User Profile " />
+            <div className="stats-box">
+              <div className="stats-menu">...</div>
+              <p className="activity">Exercise</p>
               <p className="current-stats">32hrs</p>
               <p className="prev-stats">Last Week - 10hrs</p>
             </div>
