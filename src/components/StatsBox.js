fd7c9c8392eb46d083e2a4f7742373db95416c4a
Francis Shonubi <shonubioladipo@gmail.com>
2021-12-24 20:08:52 +0100

    refactor:Made the box hover functionality pure css
    - Change the stats-box hover functionality to be in pure css
    instead of the implementation in js earlier

--- src/components/StatsBox.js
+++ src/components/StatsBox.js
@@ -6,23 +6,9 @@ const StatsBox = ({data, currentTimeline}) => {
   currentTimeline = currentTimeline.timeframe;// get the current timeline to use to populate the data
   const currentStats = data.timeframes[currentTimeline];
 
-  const boxIsHovered = (e) => {
-    let boxHovered = e.currentTarget;
-    document.querySelectorAll('.stats-box').forEach((box, index) => {
-      box.style.backgroundColor = 'hsl(235, 46%, 20%)';
-    })
-    boxHovered.style.backgroundColor = 'hsl(235, 45%, 61%)';
-  }
-
-  const boxNotHovered = (e) => {
-    document.querySelectorAll('.stats-box').forEach((box, index) => {
-      box.style.backgroundColor = 'hsl(235, 46%, 20%)';
-    })
-  }
-
 
   return (
-    <div className="stats-box" onMouseEnter={boxIsHovered} onMouseLeave={boxNotHovered}>
+    <div className="stats-box">
       <div className="stats-menu" >...</div>
       <p className="activity">{activity}</p>
       <p className="current-stats">{currentStats.current}hrs</p>
